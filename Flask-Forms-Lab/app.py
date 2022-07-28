c3f7605b0e6f8ae2fd4ef0b54b3c1531b917584d
Yazeedalyan <yazeedalyan2@gmail.com>
2022-07-28 18:14:54 +0300

    ddfdfdf

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -2,20 +2,51 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for
 import random
 
 app = Flask(  # Create a flask app
+
 	__name__,
+
 	template_folder='templates',  # Name of html file folder
 	static_folder='static'  # Name of directory for static files
 )
 
 
-username = "llo2ay"
-password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+username = "yazeed"
+password = "12345"
+facebook_friends=["mohammed","oday","tareq", "ibrahem", "Fouad", "ahmad"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/',methods = ['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+		if request.method == 'GET':
+			return render_template("login.html")
+
+		else:
+			username1 = request.form['username']
+			password1 = request.form['password']
+			if username == username1 and password == request.form['password']:
+				return redirect(url_for('home'))
+		return render_template("login.html")
+
+
+
+@app.route('/home',)  # '/' for the default page
+def home():
+			return render_template("home.html",facebook_friends=facebook_friends)
+				
+
+
+@app.route('/friend_exists/<string:name>',methods = ['GET','POST'])  # '/' for the default page
+def friend_exists(name):
+			return render_template("friend_exists.html" ,n = name,facebook_friends=facebook_friends)
+
+        
+
+
+
+
+
+
+  
   
 
 
